416341bb9f0ad3e8c5381d140b701b1bc6ac9f65
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2022-10-12 19:42:03 +0530

    Time: 678 ms (43.56%), Space: 28.6 MB (28.49%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,7 +1,7 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # approach 1 works good
-        d={}
+        """d={}
         for i in range(len(nums)):
             d[nums[i]]=1
         print(d)
@@ -9,13 +9,10 @@ class Solution:
         for i in range(1,len(nums)+1):
             if i not in d:
                 return i
-        return i+1
-        #approach 2
-        # it having tle which 171/173 test cases have passed 
-        """maxi=max(nums)
-        if maxi<=0:
-            return 1
-        for i in range(maxi+1):
-            if i not in nums and i>0:
-                return i 
-        return maxi+1"""
+        return i+1"""
+        # approach 2
+        m = set(nums)
+        
+        for i in range(1, len(nums) + 2):
+            if i not in m:
+                return i
