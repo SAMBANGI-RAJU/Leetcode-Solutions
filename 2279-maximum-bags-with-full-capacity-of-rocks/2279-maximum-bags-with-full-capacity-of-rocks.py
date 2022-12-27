2f8abf1c5b006b52bb5f265136c4f31a9a5cecee
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2022-12-27 17:01:49 +0530

    Time: 943 ms (93.42%), Space: 22 MB (87.83%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,27 +1,12 @@
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-        diffBag = []
-        # determine the difference betwin max capacity and current rocks
-        for c, r in zip(capacity,rocks):
-            diffBag.append(c-r)
-        
-        diffBag.sort()
-        
-        filledbags = 0
-        #count bags that can be filed
-        for f in diffBag:
-            if f == 0:
-                filledbags += 1
-                continue
-            
-            if additionalRocks <= 0:
-                return filledbags
-            
-            if f <= additionalRocks:
-                filledbags += 1
-            additionalRocks -= f
-        
-        return filledbags
-                
-                
-        
+        for i in range(len(capacity)):
+            capacity[i] -= rocks[i]
+        capacity.sort()
+
+        ans = 0
+        for i in capacity:
+            if i <= additionalRocks:
+                additionalRocks -= i
+                ans += 1
+        return ans
