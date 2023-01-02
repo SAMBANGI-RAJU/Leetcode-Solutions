71fee12e9e3032822b56598d105cfeb6ac942dd7
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-01-02 13:41:05 +0530

    Time: 32 ms (88.30%), Space: 13.9 MB (56.30%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,6 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word==word.upper() or word==word.capitalize() or word==word.lower():
+            return True 
+        return False
+        
