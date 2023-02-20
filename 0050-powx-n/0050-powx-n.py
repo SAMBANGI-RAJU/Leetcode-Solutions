1df7cf212b85b4968ee526dfe5c653c03ef83c53
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-02-20 23:52:10 +0530

    Time: 29 ms (83.73%), Space: 13.8 MB (56.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,5 +1,23 @@
 import math 
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
-        
+#         if n<0:
+#             return 1/x*self.myPow(x,-n)
+#         if n==0:
+#             return 1 
+#         elif n % 2 == 0:
+#             return self.myPow(x * x, n / 2)
+#         # Odd Power
+#         else:
+#             return x * self.myPow(x, n - 1)
+        if n == 0:
+            return 1
+        # Negative Power
+        elif n < 0:
+            return 1 / self.myPow(x, -n)
+        # Even Power
+        elif n % 2 == 0:
+            return self.myPow(x * x, n / 2)
+        # Odd Power
+        else:
+            return x * self.myPow(x, n - 1)
