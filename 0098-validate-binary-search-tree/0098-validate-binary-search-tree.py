d14855477a4f70d103a686a12b3f66cee12de232
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-05-29 21:25:46 +0530

    Time: 56 ms (43.84%), Space: 19.6 MB (6.93%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,16 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs_inorder(self, root):
-        if not root:
-            return []
-        else:
-            return self.dfs_inorder(root.left) + [root.val] + self.dfs_inorder(root.right)
-    
-    
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        tree_elements = self.dfs_inorder(root)
-        for i in range(1, len(tree_elements)):
-            if tree_elements[i] <= tree_elements[i - 1]:
-                return False
+        res=[]
+        def inorder(root):
+            if not root:
+                return 
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        for i in range(1,len(res)):
+            if res[i]<=res[i-1]:
+                return False 
         return True
+        
