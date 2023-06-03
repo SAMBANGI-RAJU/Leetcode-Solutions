9fd20c40c407fa3f09ca395eb45e6b9511b9d7e0
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-06-03 11:39:56 +0530

    Time: 286 ms (96.33%), Space: 18.9 MB (32.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
-        sum = 0
-        cumulativeSumFreq = {}
-
+        cnt=0
+        sum=0
+        diff=0
+        d={0:1}
         for num in nums:
-            sum += num
-            if sum == k:
-                count += 1
-            if sum - k in cumulativeSumFreq:
-                count += cumulativeSumFreq[sum - k]
-            if sum in cumulativeSumFreq:
-                cumulativeSumFreq[sum] += 1
+            sum+=num
+            diff=sum-k
+            if diff in d:
+                cnt+=d[diff]
+            if sum in d:
+                d[sum]+=1 
             else:
-                cumulativeSumFreq[sum] = 1
-
-        return count
+                d[sum]=1
+    
+        return cnt
