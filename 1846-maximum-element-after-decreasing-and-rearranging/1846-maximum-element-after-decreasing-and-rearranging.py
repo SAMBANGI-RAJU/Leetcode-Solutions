95c25064f3cfcdfa6e65e99500640ac80cb422e7
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-11-15 19:09:50 +0530

    Time: 438 ms (30.94%), Space: 26.4 MB (21.55%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -7,10 +7,8 @@ class Solution:
         if arr[0] != 1:
             arr[0] = 1
 
-        for j in range(1, n):
-            if arr[j] - arr[j - 1] <= 1:
-                continue
-            else:
-                arr[j] = 1 + arr[j - 1]
+        for i in range(1, n):
+            if not arr[i] - arr[i - 1] <= 1:
+                arr[i] = 1 + arr[i - 1]
 
         return max(arr)
