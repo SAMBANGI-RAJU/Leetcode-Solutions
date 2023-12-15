ce867c2742577ec24ccadf9252c70bc388cfa179
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-12-15 16:48:56 +0530

    Time: 64 ms (43.94%), Space: 16.3 MB (74.58%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,7 +1,8 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
         paths=dict(paths)
-        print(paths)
+       # print(paths)
         for i in paths:
+            #print(i)
             if paths[i] not in paths:
                 return paths[i]
