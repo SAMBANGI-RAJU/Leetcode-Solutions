f61810823aa1a757009384884f139aff3dd29d76
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2024-02-07 17:34:46 +0530

    Time: 41 ms (84.12%), Space: 17.8 MB (71.88%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,14 +1,20 @@
+from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
-        d={}
-        for i in s:
-            if i not in d:
-                d[i]=1 
-            else:
-                d[i]+=1 
-        sorted_dict = dict(sorted(d.items(), reverse=True,key=lambda item: item[1]))
+        # d={}
+        # for i in s:
+        #     if i not in d:
+        #         d[i]=1 
+        #     else:
+        #         d[i]+=1 
+        # sorted_dict = dict(sorted(d.items(), reverse=True,key=lambda item: item[1]))
+        # res=''
+        # for k,v in sorted_dict.items():
+        #     res+=(k*v)
+        # return res
         res=''
-        for k,v in sorted_dict.items():
+        d=Counter(s)
+        for k,v in d.most_common():
             res+=(k*v)
         return res
 
