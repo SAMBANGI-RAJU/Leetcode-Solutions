698288176779b314e8789b773ee0902210f4d99c
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2024-02-12 10:00:44 +0530

    Time: 136 ms (61.11%), Space: 18.1 MB (63.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,13 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # a=Counter(nums)
-        # """a={}
-        # for i in range(len(nums)):
-        #     if nums[i] not in a:
-        #         a[nums[i]]=1
-        #     else:
-        #         a[nums[i]]+=1 
-        # re=max(a.values())"""
-        # temp=0
-        # for k,v in a.items():
-        #     if v>temp:
-        #         res=k 
-        #         temp=v 
-        # return res
-        return sorted(nums)[len(nums)//2]
+        d=Counter(nums)
+        maxi=-10**9
+        ans=0
+        for k,v in d.items():
+            if v>maxi:
+                ans=k
+                maxi=v 
+        return ans
+        
+        
